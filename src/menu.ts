a0fe6af0f1381a1d294f87975b1c3a9c29f52eca
Douglas Alves <douglasmonttozo@hotmail.com.br>
2023-11-13 09:06:13 -0300

    arrumando conflito

--- src/menu.ts
+++
@@ -1,6 +0,0 @@
-const btnCadastro = document.getElementById("btn-cadastro");
-
-async function toggleMenu(event: PointerEvent) {
-  console.log(event);
-}
-btnCadastro?.addEventListener("pointerdown", toggleMenu);
