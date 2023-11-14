38b3cd1250771d2db862ad54e058d1501e636215
Douglas Alves <douglasmonttozo@hotmail.com.br>
2023-11-13 22:02:17 -0300

    Tabela responsiva

--- public/scripts/menu.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const btnCadastro = document.getElementById("btn-cadastro");
-async function toggleMenu(event) {
-    console.log(event);
-}
-btnCadastro?.addEventListener("pointerdown", toggleMenu);
